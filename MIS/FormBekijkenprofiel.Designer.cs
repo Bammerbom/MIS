9d677ce457cdc8741584deb5462c61f05deef693
niels <nielsvdwal.27@gmail.com>
2018-01-09 10:59:25 +0100

    lpol

--- MIS/FormBekijkenprofiel.Designer.cs
+++ MIS/FormBekijkenprofiel.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MIS
 {
-    partial class Form1
+    partial class FormBekijkenprofiel
     {
         /// <summary>
         /// Required designer variable.
@@ -28,11 +28,306 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SDierLabel = new System.Windows.Forms.Label();
+            this.LocatieLabel = new System.Windows.Forms.Label();
+            this.VogelCheckBox = new System.Windows.Forms.CheckBox();
+            this.ReptielAmfibieCheckBox = new System.Windows.Forms.CheckBox();
+            this.KnaagdierCheckBox = new System.Windows.Forms.CheckBox();
+            this.HondKatCheckBox = new System.Windows.Forms.CheckBox();
+            this.ZoektextBox = new System.Windows.Forms.TextBox();
+            this.ZoekButton = new System.Windows.Forms.Button();
+            this.@__Label = new System.Windows.Forms.Label();
+            this.TotLabel = new System.Windows.Forms.Label();
+            this.VanLabel = new System.Windows.Forms.Label();
+            this.dateTimePickerTot = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerVan = new System.Windows.Forms.DateTimePicker();
+            this.AndersCheckBox = new System.Windows.Forms.CheckBox();
+            this.InsectCheckBox = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Profielfoto = new System.Windows.Forms.PictureBox();
+            this.Naam = new System.Windows.Forms.Label();
+            this.Test = new System.Windows.Forms.PictureBox();
+            this.Vertified = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Profielfoto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Test)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Vertified)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SDierLabel
+            // 
+            this.SDierLabel.AutoSize = true;
+            this.SDierLabel.Font = new System.Drawing.Font("Tw Cen MT Condensed Extra Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SDierLabel.Location = new System.Drawing.Point(506, 24);
+            this.SDierLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.SDierLabel.Name = "SDierLabel";
+            this.SDierLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.SDierLabel.Size = new System.Drawing.Size(78, 21);
+            this.SDierLabel.TabIndex = 29;
+            this.SDierLabel.Text = "Soort Dier:";
+            // 
+            // LocatieLabel
+            // 
+            this.LocatieLabel.AutoSize = true;
+            this.LocatieLabel.Font = new System.Drawing.Font("Tw Cen MT Condensed Extra Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LocatieLabel.Location = new System.Drawing.Point(15, 28);
+            this.LocatieLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LocatieLabel.Name = "LocatieLabel";
+            this.LocatieLabel.Size = new System.Drawing.Size(53, 21);
+            this.LocatieLabel.TabIndex = 28;
+            this.LocatieLabel.Text = "Locatie";
+            // 
+            // VogelCheckBox
+            // 
+            this.VogelCheckBox.AutoSize = true;
+            this.VogelCheckBox.Font = new System.Drawing.Font("Tw Cen MT Condensed", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VogelCheckBox.Location = new System.Drawing.Point(590, 81);
+            this.VogelCheckBox.Margin = new System.Windows.Forms.Padding(4);
+            this.VogelCheckBox.Name = "VogelCheckBox";
+            this.VogelCheckBox.Size = new System.Drawing.Size(58, 24);
+            this.VogelCheckBox.TabIndex = 25;
+            this.VogelCheckBox.Text = "Vogel";
+            this.VogelCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // ReptielAmfibieCheckBox
+            // 
+            this.ReptielAmfibieCheckBox.AutoSize = true;
+            this.ReptielAmfibieCheckBox.Font = new System.Drawing.Font("Tw Cen MT Condensed", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ReptielAmfibieCheckBox.Location = new System.Drawing.Point(712, 20);
+            this.ReptielAmfibieCheckBox.Margin = new System.Windows.Forms.Padding(4);
+            this.ReptielAmfibieCheckBox.Name = "ReptielAmfibieCheckBox";
+            this.ReptielAmfibieCheckBox.Size = new System.Drawing.Size(108, 24);
+            this.ReptielAmfibieCheckBox.TabIndex = 24;
+            this.ReptielAmfibieCheckBox.Text = "Reptiel/Amfibie";
+            this.ReptielAmfibieCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // KnaagdierCheckBox
+            // 
+            this.KnaagdierCheckBox.AutoSize = true;
+            this.KnaagdierCheckBox.Font = new System.Drawing.Font("Tw Cen MT Condensed", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KnaagdierCheckBox.Location = new System.Drawing.Point(590, 52);
+            this.KnaagdierCheckBox.Margin = new System.Windows.Forms.Padding(4);
+            this.KnaagdierCheckBox.Name = "KnaagdierCheckBox";
+            this.KnaagdierCheckBox.Size = new System.Drawing.Size(82, 24);
+            this.KnaagdierCheckBox.TabIndex = 23;
+            this.KnaagdierCheckBox.Text = "Knaagdier";
+            this.KnaagdierCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // HondKatCheckBox
+            // 
+            this.HondKatCheckBox.AutoSize = true;
+            this.HondKatCheckBox.Font = new System.Drawing.Font("Tw Cen MT Condensed", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HondKatCheckBox.Location = new System.Drawing.Point(590, 24);
+            this.HondKatCheckBox.Margin = new System.Windows.Forms.Padding(4);
+            this.HondKatCheckBox.Name = "HondKatCheckBox";
+            this.HondKatCheckBox.Size = new System.Drawing.Size(78, 24);
+            this.HondKatCheckBox.TabIndex = 22;
+            this.HondKatCheckBox.Text = "Hond/Kat";
+            this.HondKatCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // ZoektextBox
+            // 
+            this.ZoektextBox.Font = new System.Drawing.Font("Tw Cen MT Condensed Extra Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ZoektextBox.Location = new System.Drawing.Point(79, 24);
+            this.ZoektextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.ZoektextBox.Name = "ZoektextBox";
+            this.ZoektextBox.Size = new System.Drawing.Size(273, 26);
+            this.ZoektextBox.TabIndex = 21;
+            // 
+            // ZoekButton
+            // 
+            this.ZoekButton.Font = new System.Drawing.Font("Tw Cen MT Condensed Extra Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ZoekButton.Location = new System.Drawing.Point(350, 23);
+            this.ZoekButton.Margin = new System.Windows.Forms.Padding(4);
+            this.ZoekButton.Name = "ZoekButton";
+            this.ZoekButton.Size = new System.Drawing.Size(100, 28);
+            this.ZoekButton.TabIndex = 20;
+            this.ZoekButton.Text = "Zoek";
+            this.ZoekButton.UseVisualStyleBackColor = true;
+            // 
+            // __Label
+            // 
+            this.@__Label.AutoSize = true;
+            this.@__Label.Font = new System.Drawing.Font("Tw Cen MT Condensed Extra Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.@__Label.Location = new System.Drawing.Point(236, 82);
+            this.@__Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.@__Label.Name = "__Label";
+            this.@__Label.Size = new System.Drawing.Size(40, 21);
+            this.@__Label.TabIndex = 19;
+            this.@__Label.Text = "------";
+            // 
+            // TotLabel
+            // 
+            this.TotLabel.AutoSize = true;
+            this.TotLabel.Font = new System.Drawing.Font("Tw Cen MT Condensed Extra Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TotLabel.Location = new System.Drawing.Point(282, 77);
+            this.TotLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.TotLabel.Name = "TotLabel";
+            this.TotLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.TotLabel.Size = new System.Drawing.Size(34, 21);
+            this.TotLabel.TabIndex = 18;
+            this.TotLabel.Text = "Tot:";
+            // 
+            // VanLabel
+            // 
+            this.VanLabel.AutoSize = true;
+            this.VanLabel.Font = new System.Drawing.Font("Tw Cen MT Condensed Extra Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VanLabel.Location = new System.Drawing.Point(20, 77);
+            this.VanLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.VanLabel.Name = "VanLabel";
+            this.VanLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.VanLabel.Size = new System.Drawing.Size(40, 21);
+            this.VanLabel.TabIndex = 17;
+            this.VanLabel.Text = "Van:";
+            // 
+            // dateTimePickerTot
+            // 
+            this.dateTimePickerTot.Font = new System.Drawing.Font("Tw Cen MT Condensed", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePickerTot.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerTot.Location = new System.Drawing.Point(323, 77);
+            this.dateTimePickerTot.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePickerTot.Name = "dateTimePickerTot";
+            this.dateTimePickerTot.Size = new System.Drawing.Size(131, 26);
+            this.dateTimePickerTot.TabIndex = 16;
+            // 
+            // dateTimePickerVan
+            // 
+            this.dateTimePickerVan.Font = new System.Drawing.Font("Tw Cen MT Condensed", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePickerVan.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerVan.Location = new System.Drawing.Point(79, 77);
+            this.dateTimePickerVan.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePickerVan.Name = "dateTimePickerVan";
+            this.dateTimePickerVan.Size = new System.Drawing.Size(131, 26);
+            this.dateTimePickerVan.TabIndex = 15;
+            // 
+            // AndersCheckBox
+            // 
+            this.AndersCheckBox.AutoSize = true;
+            this.AndersCheckBox.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.AndersCheckBox.Font = new System.Drawing.Font("Tw Cen MT Condensed", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AndersCheckBox.Location = new System.Drawing.Point(712, 81);
+            this.AndersCheckBox.Margin = new System.Windows.Forms.Padding(4);
+            this.AndersCheckBox.Name = "AndersCheckBox";
+            this.AndersCheckBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.AndersCheckBox.Size = new System.Drawing.Size(66, 24);
+            this.AndersCheckBox.TabIndex = 31;
+            this.AndersCheckBox.Text = "Anders";
+            this.AndersCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // InsectCheckBox
+            // 
+            this.InsectCheckBox.AutoSize = true;
+            this.InsectCheckBox.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.InsectCheckBox.Font = new System.Drawing.Font("Tw Cen MT Condensed", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InsectCheckBox.Location = new System.Drawing.Point(712, 52);
+            this.InsectCheckBox.Margin = new System.Windows.Forms.Padding(4);
+            this.InsectCheckBox.Name = "InsectCheckBox";
+            this.InsectCheckBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.InsectCheckBox.Size = new System.Drawing.Size(61, 24);
+            this.InsectCheckBox.TabIndex = 30;
+            this.InsectCheckBox.Text = "Insect";
+            this.InsectCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-8, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1496, 17);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "_________________________________________________________________________________" +
+    "________________________________________________________________________________" +
+    "_________________________";
+            // 
+            // Profielfoto
+            // 
+            this.Profielfoto.Location = new System.Drawing.Point(58, 189);
+            this.Profielfoto.Name = "Profielfoto";
+            this.Profielfoto.Size = new System.Drawing.Size(225, 207);
+            this.Profielfoto.TabIndex = 33;
+            this.Profielfoto.TabStop = false;
+            // 
+            // Naam
+            // 
+            this.Naam.AutoSize = true;
+            this.Naam.Font = new System.Drawing.Font("Tw Cen MT Condensed", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Naam.Location = new System.Drawing.Point(345, 189);
+            this.Naam.Name = "Naam";
+            this.Naam.Size = new System.Drawing.Size(188, 28);
+            this.Naam.TabIndex = 34;
+            this.Naam.Text = "Naam + achternaam";
+            // 
+            // Test
+            // 
+            this.Test.Location = new System.Drawing.Point(601, 190);
+            this.Test.Name = "Test";
+            this.Test.Size = new System.Drawing.Size(32, 27);
+            this.Test.TabIndex = 35;
+            this.Test.TabStop = false;
+            // 
+            // Vertified
+            // 
+            this.Vertified.Location = new System.Drawing.Point(554, 189);
+            this.Vertified.Name = "Vertified";
+            this.Vertified.Size = new System.Drawing.Size(30, 28);
+            this.Vertified.TabIndex = 36;
+            this.Vertified.TabStop = false;
+            // 
+            // FormBekijkenprofiel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1312, 814);
+            this.Controls.Add(this.Vertified);
+            this.Controls.Add(this.Test);
+            this.Controls.Add(this.Naam);
+            this.Controls.Add(this.Profielfoto);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AndersCheckBox);
+            this.Controls.Add(this.InsectCheckBox);
+            this.Controls.Add(this.SDierLabel);
+            this.Controls.Add(this.LocatieLabel);
+            this.Controls.Add(this.VogelCheckBox);
+            this.Controls.Add(this.ReptielAmfibieCheckBox);
+            this.Controls.Add(this.KnaagdierCheckBox);
+            this.Controls.Add(this.HondKatCheckBox);
+            this.Controls.Add(this.ZoektextBox);
+            this.Controls.Add(this.ZoekButton);
+            this.Controls.Add(this.@__Label);
+            this.Controls.Add(this.TotLabel);
+            this.Controls.Add(this.VanLabel);
+            this.Controls.Add(this.dateTimePickerTot);
+            this.Controls.Add(this.dateTimePickerVan);
+            this.Name = "FormBekijkenprofiel";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Profielfoto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Test)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Vertified)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label SDierLabel;
+        private System.Windows.Forms.Label LocatieLabel;
+        private System.Windows.Forms.CheckBox VogelCheckBox;
+        private System.Windows.Forms.CheckBox ReptielAmfibieCheckBox;
+        private System.Windows.Forms.CheckBox KnaagdierCheckBox;
+        private System.Windows.Forms.CheckBox HondKatCheckBox;
+        private System.Windows.Forms.TextBox ZoektextBox;
+        private System.Windows.Forms.Button ZoekButton;
+        private System.Windows.Forms.Label __Label;
+        private System.Windows.Forms.Label TotLabel;
+        private System.Windows.Forms.Label VanLabel;
+        private System.Windows.Forms.DateTimePicker dateTimePickerTot;
+        private System.Windows.Forms.DateTimePicker dateTimePickerVan;
+        private System.Windows.Forms.CheckBox AndersCheckBox;
+        private System.Windows.Forms.CheckBox InsectCheckBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox Profielfoto;
+        private System.Windows.Forms.Label Naam;
+        private System.Windows.Forms.PictureBox Test;
+        private System.Windows.Forms.PictureBox Vertified;
     }
 }
--- MIS/FormBekijkenprofiel.cs
+++ MIS/FormBekijkenprofiel.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace MIS
 {
-    public partial class Form1 : Form
+    public partial class FormBekijkenprofiel : Form
     {
-        public Form1()
+        public FormBekijkenprofiel()
         {
             InitializeComponent();
         }

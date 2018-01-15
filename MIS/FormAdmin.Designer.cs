32a8ea063287a83257a2529ea46dff54c900b39c
ArjanWedman <wedm1600@student.nhl.nl>
2018-01-15 15:17:00 +0100

    Editen van rows (BUGGY AF)

--- MIS/FormAdmin.Designer.cs
+++ MIS/FormAdmin.Designer.cs
@@ -29,8 +29,34 @@
         private void InitializeComponent()
         {
             this.AdminDatagrid = new System.Windows.Forms.DataGridView();
-            this.EditButton = new System.Windows.Forms.Button();
-            this.ReadButton = new System.Windows.Forms.Button();
+            this.VoornaamAdmin = new System.Windows.Forms.TextBox();
+            this.AchternaamAdmin = new System.Windows.Forms.TextBox();
+            this.WoonplaatsAdmin = new System.Windows.Forms.TextBox();
+            this.VraagprijsAdmin = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.AdminAdminLabel = new System.Windows.Forms.Label();
+            this.VerifiedAdmin = new System.Windows.Forms.CheckBox();
+            this.AdminAdmin = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.OppassenAdmin = new System.Windows.Forms.CheckBox();
+            this.UitlatenAdmin = new System.Windows.Forms.CheckBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.RatingAdmin = new System.Windows.Forms.TextBox();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.AmfibieCheckboxAdmin = new System.Windows.Forms.CheckBox();
+            this.KatCheckboxAdmin = new System.Windows.Forms.CheckBox();
+            this.VissenCheckboxAdmin = new System.Windows.Forms.CheckBox();
+            this.InsectCheckboxAdmin = new System.Windows.Forms.CheckBox();
+            this.VogelCheckboxAdmin = new System.Windows.Forms.CheckBox();
+            this.ReptielCheckboxAdmin = new System.Windows.Forms.CheckBox();
+            this.KnaagdierCheckboxAdmin = new System.Windows.Forms.CheckBox();
+            this.HondCheckboxAdmin = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.AdminDatagrid)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,50 +70,335 @@
             this.AdminDatagrid.Location = new System.Drawing.Point(13, 13);
             this.AdminDatagrid.Name = "AdminDatagrid";
             this.AdminDatagrid.ReadOnly = true;
-            this.AdminDatagrid.Size = new System.Drawing.Size(1159, 690);
+            this.AdminDatagrid.Size = new System.Drawing.Size(1159, 736);
             this.AdminDatagrid.TabIndex = 0;
             this.AdminDatagrid.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.AdminDatagrid_CellMouseClick);
+            this.AdminDatagrid.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.AdminDatagrid_RowHeaderMouseClick_1);
             // 
-            // EditButton
+            // VoornaamAdmin
             // 
-            this.EditButton.Location = new System.Drawing.Point(1016, 709);
-            this.EditButton.Name = "EditButton";
-            this.EditButton.Size = new System.Drawing.Size(75, 23);
-            this.EditButton.TabIndex = 1;
-            this.EditButton.Text = "Edit Data";
-            this.EditButton.UseVisualStyleBackColor = true;
-            this.EditButton.Click += new System.EventHandler(this.EditButton_Click);
+            this.VoornaamAdmin.Location = new System.Drawing.Point(1272, 12);
+            this.VoornaamAdmin.Name = "VoornaamAdmin";
+            this.VoornaamAdmin.Size = new System.Drawing.Size(100, 20);
+            this.VoornaamAdmin.TabIndex = 3;
             // 
-            // ReadButton
+            // AchternaamAdmin
             // 
-            this.ReadButton.Location = new System.Drawing.Point(1097, 709);
-            this.ReadButton.Name = "ReadButton";
-            this.ReadButton.Size = new System.Drawing.Size(75, 23);
-            this.ReadButton.TabIndex = 2;
-            this.ReadButton.Text = "Read Only";
-            this.ReadButton.UseVisualStyleBackColor = true;
-            this.ReadButton.Click += new System.EventHandler(this.ReadButton_Click);
+            this.AchternaamAdmin.Location = new System.Drawing.Point(1272, 38);
+            this.AchternaamAdmin.Name = "AchternaamAdmin";
+            this.AchternaamAdmin.Size = new System.Drawing.Size(100, 20);
+            this.AchternaamAdmin.TabIndex = 4;
+            // 
+            // WoonplaatsAdmin
+            // 
+            this.WoonplaatsAdmin.Location = new System.Drawing.Point(1272, 64);
+            this.WoonplaatsAdmin.Name = "WoonplaatsAdmin";
+            this.WoonplaatsAdmin.Size = new System.Drawing.Size(100, 20);
+            this.WoonplaatsAdmin.TabIndex = 5;
+            // 
+            // VraagprijsAdmin
+            // 
+            this.VraagprijsAdmin.Location = new System.Drawing.Point(1272, 90);
+            this.VraagprijsAdmin.Name = "VraagprijsAdmin";
+            this.VraagprijsAdmin.Size = new System.Drawing.Size(100, 20);
+            this.VraagprijsAdmin.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1178, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Voornaam";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1178, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Achternaam";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1178, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Woonplaats";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1178, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Vraagprijs";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1181, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Verified";
+            // 
+            // AdminAdminLabel
+            // 
+            this.AdminAdminLabel.AutoSize = true;
+            this.AdminAdminLabel.Location = new System.Drawing.Point(1269, 131);
+            this.AdminAdminLabel.Name = "AdminAdminLabel";
+            this.AdminAdminLabel.Size = new System.Drawing.Size(36, 13);
+            this.AdminAdminLabel.TabIndex = 12;
+            this.AdminAdminLabel.Text = "Admin";
+            // 
+            // VerifiedAdmin
+            // 
+            this.VerifiedAdmin.AutoSize = true;
+            this.VerifiedAdmin.Location = new System.Drawing.Point(1242, 132);
+            this.VerifiedAdmin.Name = "VerifiedAdmin";
+            this.VerifiedAdmin.Size = new System.Drawing.Size(15, 14);
+            this.VerifiedAdmin.TabIndex = 13;
+            this.VerifiedAdmin.UseVisualStyleBackColor = true;
+            // 
+            // AdminAdmin
+            // 
+            this.AdminAdmin.AutoSize = true;
+            this.AdminAdmin.Location = new System.Drawing.Point(1318, 132);
+            this.AdminAdmin.Name = "AdminAdmin";
+            this.AdminAdmin.Size = new System.Drawing.Size(15, 14);
+            this.AdminAdmin.TabIndex = 14;
+            this.AdminAdmin.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1181, 183);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Oppassen";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1269, 183);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Uitlaten";
+            // 
+            // OppassenAdmin
+            // 
+            this.OppassenAdmin.AutoSize = true;
+            this.OppassenAdmin.Location = new System.Drawing.Point(1242, 183);
+            this.OppassenAdmin.Name = "OppassenAdmin";
+            this.OppassenAdmin.Size = new System.Drawing.Size(15, 14);
+            this.OppassenAdmin.TabIndex = 17;
+            this.OppassenAdmin.UseVisualStyleBackColor = true;
+            // 
+            // UitlatenAdmin
+            // 
+            this.UitlatenAdmin.AutoSize = true;
+            this.UitlatenAdmin.Location = new System.Drawing.Point(1318, 182);
+            this.UitlatenAdmin.Name = "UitlatenAdmin";
+            this.UitlatenAdmin.Size = new System.Drawing.Size(15, 14);
+            this.UitlatenAdmin.TabIndex = 18;
+            this.UitlatenAdmin.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(1181, 233);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Diertypes";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(1178, 471);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(38, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Rating";
+            // 
+            // RatingAdmin
+            // 
+            this.RatingAdmin.Location = new System.Drawing.Point(1272, 464);
+            this.RatingAdmin.Name = "RatingAdmin";
+            this.RatingAdmin.Size = new System.Drawing.Size(100, 20);
+            this.RatingAdmin.TabIndex = 21;
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.Location = new System.Drawing.Point(1297, 726);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(75, 23);
+            this.UpdateButton.TabIndex = 22;
+            this.UpdateButton.Text = "Update";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
+            // 
+            // AmfibieCheckboxAdmin
+            // 
+            this.AmfibieCheckboxAdmin.AutoSize = true;
+            this.AmfibieCheckboxAdmin.Location = new System.Drawing.Point(1184, 373);
+            this.AmfibieCheckboxAdmin.Name = "AmfibieCheckboxAdmin";
+            this.AmfibieCheckboxAdmin.Size = new System.Drawing.Size(60, 17);
+            this.AmfibieCheckboxAdmin.TabIndex = 42;
+            this.AmfibieCheckboxAdmin.Text = "Amfibie";
+            this.AmfibieCheckboxAdmin.UseVisualStyleBackColor = true;
+            // 
+            // KatCheckboxAdmin
+            // 
+            this.KatCheckboxAdmin.AutoSize = true;
+            this.KatCheckboxAdmin.Location = new System.Drawing.Point(1184, 281);
+            this.KatCheckboxAdmin.Name = "KatCheckboxAdmin";
+            this.KatCheckboxAdmin.Size = new System.Drawing.Size(42, 17);
+            this.KatCheckboxAdmin.TabIndex = 41;
+            this.KatCheckboxAdmin.Text = "Kat";
+            this.KatCheckboxAdmin.UseVisualStyleBackColor = true;
+            // 
+            // VissenCheckboxAdmin
+            // 
+            this.VissenCheckboxAdmin.AutoSize = true;
+            this.VissenCheckboxAdmin.Location = new System.Drawing.Point(1184, 419);
+            this.VissenCheckboxAdmin.Name = "VissenCheckboxAdmin";
+            this.VissenCheckboxAdmin.Size = new System.Drawing.Size(57, 17);
+            this.VissenCheckboxAdmin.TabIndex = 40;
+            this.VissenCheckboxAdmin.Text = "Vissen";
+            this.VissenCheckboxAdmin.UseVisualStyleBackColor = true;
+            // 
+            // InsectCheckboxAdmin
+            // 
+            this.InsectCheckboxAdmin.AutoSize = true;
+            this.InsectCheckboxAdmin.Location = new System.Drawing.Point(1184, 396);
+            this.InsectCheckboxAdmin.Name = "InsectCheckboxAdmin";
+            this.InsectCheckboxAdmin.Size = new System.Drawing.Size(55, 17);
+            this.InsectCheckboxAdmin.TabIndex = 39;
+            this.InsectCheckboxAdmin.Text = "Insect";
+            this.InsectCheckboxAdmin.UseVisualStyleBackColor = true;
+            // 
+            // VogelCheckboxAdmin
+            // 
+            this.VogelCheckboxAdmin.AutoSize = true;
+            this.VogelCheckboxAdmin.Location = new System.Drawing.Point(1184, 327);
+            this.VogelCheckboxAdmin.Name = "VogelCheckboxAdmin";
+            this.VogelCheckboxAdmin.Size = new System.Drawing.Size(53, 17);
+            this.VogelCheckboxAdmin.TabIndex = 38;
+            this.VogelCheckboxAdmin.Text = "Vogel";
+            this.VogelCheckboxAdmin.UseVisualStyleBackColor = true;
+            // 
+            // ReptielCheckboxAdmin
+            // 
+            this.ReptielCheckboxAdmin.AutoSize = true;
+            this.ReptielCheckboxAdmin.Location = new System.Drawing.Point(1184, 350);
+            this.ReptielCheckboxAdmin.Name = "ReptielCheckboxAdmin";
+            this.ReptielCheckboxAdmin.Size = new System.Drawing.Size(59, 17);
+            this.ReptielCheckboxAdmin.TabIndex = 37;
+            this.ReptielCheckboxAdmin.Text = "Reptiel";
+            this.ReptielCheckboxAdmin.UseVisualStyleBackColor = true;
+            // 
+            // KnaagdierCheckboxAdmin
+            // 
+            this.KnaagdierCheckboxAdmin.AutoSize = true;
+            this.KnaagdierCheckboxAdmin.Location = new System.Drawing.Point(1184, 304);
+            this.KnaagdierCheckboxAdmin.Name = "KnaagdierCheckboxAdmin";
+            this.KnaagdierCheckboxAdmin.Size = new System.Drawing.Size(74, 17);
+            this.KnaagdierCheckboxAdmin.TabIndex = 36;
+            this.KnaagdierCheckboxAdmin.Text = "Knaagdier";
+            this.KnaagdierCheckboxAdmin.UseVisualStyleBackColor = true;
+            // 
+            // HondCheckboxAdmin
+            // 
+            this.HondCheckboxAdmin.AutoSize = true;
+            this.HondCheckboxAdmin.Location = new System.Drawing.Point(1184, 258);
+            this.HondCheckboxAdmin.Name = "HondCheckboxAdmin";
+            this.HondCheckboxAdmin.Size = new System.Drawing.Size(52, 17);
+            this.HondCheckboxAdmin.TabIndex = 35;
+            this.HondCheckboxAdmin.Text = "Hond";
+            this.HondCheckboxAdmin.UseVisualStyleBackColor = true;
             // 
             // FormAdmin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1184, 761);
-            this.Controls.Add(this.ReadButton);
-            this.Controls.Add(this.EditButton);
+            this.ClientSize = new System.Drawing.Size(1384, 761);
+            this.Controls.Add(this.AmfibieCheckboxAdmin);
+            this.Controls.Add(this.KatCheckboxAdmin);
+            this.Controls.Add(this.VissenCheckboxAdmin);
+            this.Controls.Add(this.InsectCheckboxAdmin);
+            this.Controls.Add(this.VogelCheckboxAdmin);
+            this.Controls.Add(this.ReptielCheckboxAdmin);
+            this.Controls.Add(this.KnaagdierCheckboxAdmin);
+            this.Controls.Add(this.HondCheckboxAdmin);
+            this.Controls.Add(this.UpdateButton);
+            this.Controls.Add(this.RatingAdmin);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.UitlatenAdmin);
+            this.Controls.Add(this.OppassenAdmin);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.AdminAdmin);
+            this.Controls.Add(this.VerifiedAdmin);
+            this.Controls.Add(this.AdminAdminLabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.VraagprijsAdmin);
+            this.Controls.Add(this.WoonplaatsAdmin);
+            this.Controls.Add(this.AchternaamAdmin);
+            this.Controls.Add(this.VoornaamAdmin);
             this.Controls.Add(this.AdminDatagrid);
             this.Name = "FormAdmin";
-            this.Text = "Admin pagina";
+            this.Text = "FormAdmin";
             this.Load += new System.EventHandler(this.FormAdmin_Load);
             ((System.ComponentModel.ISupportInitialize)(this.AdminDatagrid)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView AdminDatagrid;
-        private System.Windows.Forms.Button EditButton;
-        private System.Windows.Forms.Button ReadButton;
+        private System.Windows.Forms.TextBox VoornaamAdmin;
+        private System.Windows.Forms.TextBox AchternaamAdmin;
+        private System.Windows.Forms.TextBox WoonplaatsAdmin;
+        private System.Windows.Forms.TextBox VraagprijsAdmin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label AdminAdminLabel;
+        private System.Windows.Forms.CheckBox VerifiedAdmin;
+        private System.Windows.Forms.CheckBox AdminAdmin;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox OppassenAdmin;
+        private System.Windows.Forms.CheckBox UitlatenAdmin;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox RatingAdmin;
+        private System.Windows.Forms.Button UpdateButton;
+        private System.Windows.Forms.CheckBox AmfibieCheckboxAdmin;
+        private System.Windows.Forms.CheckBox KatCheckboxAdmin;
+        private System.Windows.Forms.CheckBox VissenCheckboxAdmin;
+        private System.Windows.Forms.CheckBox InsectCheckboxAdmin;
+        private System.Windows.Forms.CheckBox VogelCheckboxAdmin;
+        private System.Windows.Forms.CheckBox ReptielCheckboxAdmin;
+        private System.Windows.Forms.CheckBox KnaagdierCheckboxAdmin;
+        private System.Windows.Forms.CheckBox HondCheckboxAdmin;
     }
 }
--- MIS/FormAdmin.cs
+++ MIS/FormAdmin.cs
@@ -13,6 +13,7 @@ namespace MIS
 {
     public partial class FormAdmin : Form
     {
+        int? CurrentRow = null;
         public FormAdmin()
         {
             InitializeComponent();
@@ -31,7 +32,7 @@ namespace MIS
             AdminDatagrid.Columns[7].Width = 70;
             AdminDatagrid.Columns[8].Width = 70;
             AdminDatagrid.Columns[9].Width = 160;
-            AdminDatagrid.Columns[10].Width = 50 ;
+            AdminDatagrid.Columns[10].Width = 50;
 
             var deleteButton = new DataGridViewButtonColumn();
             deleteButton.Name = "dataGridViewDeleteButton";
@@ -56,7 +57,7 @@ namespace MIS
             table.Columns.Add("uitlaten", typeof(bool));
             table.Columns.Add("diertypes", typeof(string));
             table.Columns.Add("rating", typeof(int));
-            
+
             //Data toevoegen
             foreach (var gebruiker in DatabaseManager.AlleGebruikers())
             {
@@ -73,7 +74,7 @@ namespace MIS
                 DialogResult ResultaatVerwijderen = MessageBox.Show("Weet je zeker dat je het geselecteerde profiel wilt verwijderen?", "Verwijderen Profiel", MessageBoxButtons.YesNo);
                 if (ResultaatVerwijderen == DialogResult.Yes)
                 {
-                    int userid = (int) AdminDatagrid[0, e.RowIndex].Value;
+                    int userid = (int)AdminDatagrid[0, e.RowIndex].Value;
                     DatabaseManager.GebruikerVerwijderen(userid);
                     AdminDatagrid.DataSource = GetDataTable();
                 }
@@ -83,11 +84,104 @@ namespace MIS
         private void EditButton_Click(object sender, EventArgs e)
         {
             AdminDatagrid.ReadOnly = false;
+            MessageBox.Show("Je kan nu gegevens aanpassen voor de verschillende profielen");
+
+
         }
 
         private void ReadButton_Click(object sender, EventArgs e)
         {
             AdminDatagrid.ReadOnly = true;
+            MessageBox.Show("Je kan nu geen gegevens meer aanpassen");
+        }
+        private void AdminDatagrid_RowHeaderMouseClick_1(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            CurrentRow = e.RowIndex;
+            List<CheckBox> CBListAdmin = new List<CheckBox>()
+                    { HondCheckboxAdmin, KatCheckboxAdmin, KnaagdierCheckboxAdmin, VogelCheckboxAdmin, ReptielCheckboxAdmin, AmfibieCheckboxAdmin, InsectCheckboxAdmin, VissenCheckboxAdmin};
+            VoornaamAdmin.Text = AdminDatagrid.Rows[e.RowIndex].Cells[1].Value.ToString();
+            AchternaamAdmin.Text = AdminDatagrid.Rows[e.RowIndex].Cells[2].Value.ToString();
+            WoonplaatsAdmin.Text = AdminDatagrid.Rows[e.RowIndex].Cells[3].Value.ToString();
+            VraagprijsAdmin.Text = AdminDatagrid.Rows[e.RowIndex].Cells[6].Value.ToString();
+            RatingAdmin.Text = AdminDatagrid.Rows[e.RowIndex].Cells[10].Value.ToString();
+            OppassenAdmin.Checked = (bool) AdminDatagrid.Rows[e.RowIndex].Cells[7].Value;
+            UitlatenAdmin.Checked = (bool)AdminDatagrid.Rows[e.RowIndex].Cells[8].Value;
+            VerifiedAdmin.Checked = (bool)AdminDatagrid.Rows[e.RowIndex].Cells[4].Value;
+            AdminAdmin.Checked = (bool)AdminDatagrid.Rows[e.RowIndex].Cells[5].Value;
+
+            foreach (CheckBox cb in CBListAdmin) cb.Checked = false;
+            string dieren = (string) AdminDatagrid.Rows[e.RowIndex].Cells[9].Value;
+            foreach(string dier in dieren.Split(", ".ToCharArray()))
+            {
+                switch (dier)
+                {
+                    case "Hond":
+                        HondCheckboxAdmin.Checked = true;
+                        break;
+                    case "Kat":
+                        KatCheckboxAdmin.Checked = true;
+                        break;
+                    case "Knaagdier":
+                        KnaagdierCheckboxAdmin.Checked = true;
+                        break;
+                    case "Vogel":
+                        VogelCheckboxAdmin.Checked = true;
+                        break;
+                    case "Reptiel":
+                        ReptielCheckboxAdmin.Checked = true;
+                        break;
+                    case "Amfibie":
+                        AmfibieCheckboxAdmin.Checked = true;
+                        break;
+                    case "Insect":
+                        InsectCheckboxAdmin.Checked = true;
+                        break;
+                    case "Vissen":
+                        VissenCheckboxAdmin.Checked = true;
+                        break;
+                }
+            }
+        }
+
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+            //Als de gebruiker geen rij selecteerd, ga dan zeuren
+            if (CurrentRow == null)
+            {
+                MessageBox.Show("Je hebt geen row geselecteerd");
+                return;
+            }
+
+            int currentr = (int)CurrentRow;
+            int userid = (int)AdminDatagrid[0, currentr].Value;
+            Gebruiker gebruiker = DatabaseManager.GebruikerOpvragen(userid);
+            gebruiker.voornaam = VoornaamAdmin.Text;
+            gebruiker.achternaam = AchternaamAdmin.Text;
+            gebruiker.woonplaats = WoonplaatsAdmin.Text;
+            gebruiker.vraagprijs = Convert.ToInt32(VraagprijsAdmin.Text);
+            gebruiker.rating = Convert.ToInt32(RatingAdmin.Text);
+            gebruiker.oppassen = OppassenAdmin.Checked;
+            gebruiker.uitlaten = UitlatenAdmin.Checked;
+            gebruiker.admin = AdminAdmin.Checked;
+            gebruiker.verified = VerifiedAdmin.Checked;
+
+            List<string> dierenstrings = new List<string>();
+            if (HondCheckboxAdmin.Checked) dierenstrings.Add("Hond");
+            if (KatCheckboxAdmin.Checked) dierenstrings.Add("Kat");
+            if (KnaagdierCheckboxAdmin.Checked) dierenstrings.Add("Knaagdier");
+            if (VogelCheckboxAdmin.Checked) dierenstrings.Add("Vogel");
+            if (ReptielCheckboxAdmin.Checked) dierenstrings.Add("Reptiel");
+            if (AmfibieCheckboxAdmin.Checked) dierenstrings.Add("Amfibie");
+            if (InsectCheckboxAdmin.Checked) dierenstrings.Add("Insect");
+            if (VissenCheckboxAdmin.Checked) dierenstrings.Add("Vissen");
+            string dieren = string.Join(", ", dierenstrings);
+            gebruiker.diertypes = dieren;
+
+            DatabaseManager.GebruikerWijzigen(gebruiker);
+            AdminDatagrid.DataSource = GetDataTable();
+
+            MessageBox.Show("Het profiel is bijgewerkt");
         }
     }
 }
+
